1a333d66bc148c1673f86acd5d9d8636d4739d7b
Eemeli Mustonen <eemuston@c2r3p4.hive.fi>
2023-04-26 19:17:12 +0300

    My 42 philosophers

---
+++ 42-philosophers.c
@@ -0,0 +1,42 @@
+Aristotle
+Confucius
+David Lewis
+Democritus
+Diogenes
+Epictetus
+Epicurus
+Friedrich Nietzsche
+Georg Wilhelm Friedrich Hegel
+George Berkeley
+Gilles Deleuze
+Hannah Arendt
+Immanuel Kant
+Jacques Derrida
+Jean Baudrillard
+Jean-Jacques Rousseau
+John Locke
+John Rawls
+Karl Marx
+Ludwig Wittgenstein
+Martin Heidegger
+Michel Foucault
+Niccolò Machiavelli
+Noam Chomsky
+Plato
+René Descartes
+Robert Nozick
+Søren Kierkegaard
+Theodor Adorno
+Thomas Aquinas
+Thomas Hobbes
+Walter Benjamin
+William James
+William of Ockham
+William Godwin
+Friedrich Wilhelm Joseph Schelling
+Georg Wilhelm Friedrich Hegel
+Johann Wolfgang von Goethe
+Friedrich Schiller
+Gotthold Ephraim Lessing
+Johann Georg Hamann
+Friedrich Hölderlin
